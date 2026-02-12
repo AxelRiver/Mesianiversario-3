bea710f9956a6ed71f3a8fa4d13804b6dd6dc3f0
AxelRiver <142948558+AxelRiver@users.noreply.github.com>
2026-02-12 16:20:06 -0600

    Fix cache URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = "mi-pwa-cache-v1";
 const urlsToCache = [
   "/",
-  "/index.html",
-  "/cake.html",
-  "/memorylane.html",
-  "/letter.html",
-  "/3.jpg",
-  "/1.jpg",
-  "/6.jpg",
-  "/2.jpeg",
-  "/5.jpg"
+  "ndex.html",
+  "cake.html",
+  "memorylane.html",
+  "letter.html",
+  "3.jpg",
+  "1.jpg",
+  "6.jpg",
+  "2.jpeg",
+  "5.jpg"
 ];
 
 self.addEventListener("install", event => {
